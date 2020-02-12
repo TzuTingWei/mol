aa0c714f276ef683e9dc4aa040d524c8f27e9d71
TzuTingWei <c1225221@gmail.com>
2020-02-12 13:58:30 +0100

    rev test_util with pytest

--- mol/tests/test_util.py
+++ mol/tests/test_util.py
@@ -1,4 +1,10 @@
 import mol
+import pytest
 
-def test_distance():
-	assert mol.util.distance([0,1], [0,0]) == 1
+@pytest.mark.parametrize("point1, point2, bench", [([0,1], [0,0], 1), ([0,2], [0,0], 2),])
+
+def test_distance(point1, point2, bench):
+	assert mol.util.distance(point1, point2) == pytest.approx(bench, abs=1.e-3)
+def test_distance_failure():
+	assert mol.util.distance([0],[3]) != 5
+
