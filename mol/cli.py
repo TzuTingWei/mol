f13915f66f45169db581cefc02bcadf4ca2188f8
TzuTingWei <c1225221@gmail.com>
2020-02-13 11:23:37 +0100

    edit entrypoint in setup.py and def main

--- mol/cli.py
+++ mol/cli.py
@@ -1,6 +1,15 @@
 import argparse
+from .util import read_xyz
 
-parser = argparse.ArgumentParser(description='Add some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='interger list')
+def main():
+	parser = argparse.ArgumentParser(description='A Molecule utility that reads XYZ files and calculates the distance between atoms at index1 and index2.')
+	parser.add_argument('filename', type=str, help='The XYZ file to read.')
+	parser.add_argument('index1', type=int, help='Index of the first atom.')
+	parser.add_argument('index2', type=int, help='Index of the second atom.')
 
+	args = parser.parse_args()
+	print(args)
+
+	moldata = read_xyz(args.filename)
+
+	print(moldata)
